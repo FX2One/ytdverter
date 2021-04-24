a00f0271afa3b8a8f31b33851d2f106b274078f8
FX2One <dfoxik@gmail.com>
2021-04-24 22:31:22 +0200

    downloader button

--- ytd.py
+++ ytd.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from pytube import YouTube
 
 root = Tk()
 
@@ -13,7 +14,7 @@ root.title("YouTube Downloader and Converter")
 
 # download and convert labels
 download_label = Label(root,text = 'Youtube Video Downloader', font='arial 20 bold').grid(row=0, column=0)
-converter_label = Label(root,text = 'Mp4 to Mp3 Converter', font='arial 20 bold').grid(row=2, column=0)
+converter_label = Label(root,text = 'Mp4 to Mp3 Converter', font='arial 20 bold').grid(row=3, column=0)
 
 # store URL from youtube
 yt_url = StringVar()
@@ -23,6 +24,19 @@ link_enter = Entry(root, width = 70 ,textvariable = yt_url).grid(row=1, column=1
 
 
 
+# Downloader
+def downloader():
+    video_url = YouTube(str(yt_url.get())) #get url through StringVar() and convert to string
+    video = video_url.streams.first() #get first object of a list from a given link
+    video.download() # start downloading
+    Label(root, text='your file has been downloaded', font='arial 15').grid(row=4 ,column=0)
+
+Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' ,bg = 'pale violet red', padx = 2, command = downloader).grid(row=2 ,column=0)
+
+
+
+
 
 # calling a program
 root.mainloop()
+
